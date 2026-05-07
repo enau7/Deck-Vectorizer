58fded197dd038f62cd831679655ca10ee4d4550
enau7 <crowervms@gmail.com>
2026-05-06 17:37:29 -0700

    trying to fix webdriver

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 sudo apt update
 sudo apt install -y chromium-browser
+sudo apt install -y \
+    libnss3 \
+    libgconf-2-4 \
+    libxi6 \
+    libxrender1 \
+    libxrandr2 \
+    libxss1 \
+    libasound2 \
+    libgtk-3-0
 pip install ./deck_scraper
 python backend/manage.py migrate
 python backend/manage.py
