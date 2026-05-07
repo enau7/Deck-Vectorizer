3392671f583e32d948acd688a71b9c7f63b4c48e
enau7 <crowervms@gmail.com>
2026-05-06 22:59:22 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,5 +13,5 @@ pip install ./deck_scraper
 cd backend
 python manage.py migrate
 python manage.py collectstatic
-python myapp/static/get_and_process.py
+python myapp/static/json/get_and_process.py
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 1 --threads 2
